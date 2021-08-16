fc75feae17b13688ecf03bbea84c4d60e24d17cc
shun-shun123 <shunsuke15vb@gmail.com>
2021-08-16 21:47:54 +0900

    memory layoutについて学んだ

--- StructBasics/main.cpp
+++ StructBasics/main.cpp
@@ -13,6 +13,13 @@ struct Vec3 {
     float z = 0;
 };
 
+// 合計で8byte確保する
+struct Padded {
+    int8_t a;   // これは1byte確保する
+                // CPUのアーキテクチャにもよるけど、例えばpadding3byte確保される
+    int32_t b;  // 4byte確保される
+};
+
 // structはそのサイズをコンパイル時に確定できないといけないため、宣言だけして定義をしていない段階で使ったらコンパイルエラーが起きる
 // これはC++のコンパイラがソースコードの上から順番に読んでいっているため。
 // ただし、それを指すポインタなら使える（ポインタならサイズが判明している必要はなくて、それがある場所だけ刺せばいいので）
@@ -48,9 +55,46 @@ void printVecOut(Vec3 vec) {
     cout << "vec: (" << vec.x << ", " << vec.y << ", " << vec.z << ")" << endl;
 }
 
-続きはLayoutについて学んでいく
+// 続きはLayoutについて学んでいく
+// C++には"standard layout"というものがあり、同じデータメンバーは同じようにメモリ上に配置される(例外もあるが)
+struct Vec3Sample
+{
+    float x;
+    float y;
+    float z;
+};
+
+struct Quat
+{
+    // Vec3と同じメモリレイアウトでスタート
+    float x;
+    float y;
+    float z;
+
+    float w;
+};
+
+void reinterpretStruct() {
+    cout << "=====reinterpretStruct=====" << endl;
+    Vec3Sample vec;
+    Vec3Sample* pVec = &vec;
+    Quat* pQuat = (Quat*)pVec;
+
+    // 次の操作は安全
+    pQuat->x = 1;
+    pQuat->y = 2;
+    pQuat->z = 3;
+
+    // 次の操作は危険. Vec3Sampleはwまでのメモリレイアウトを確保していない
+    pQuat->w  = 4;
+    cout << "x: " << pQuat->x << endl;
+    cout << "y: " << pQuat->y << endl;
+    cout << "z: " << pQuat->z << endl;
+    cout << "w: " << pQuat->w << endl;
+}
 
 int main() {
     useStructSample();
     anonymousStruct();
+    reinterpretStruct();
 }
